acb1c954b6633f974c066b980146122aaac3a9e1
Bobby Costin <bobby.kostin@gmail.com>
2025-12-06 00:35:29 -0500

    fix

--- code/moes.py
+++ code/moes.py
@@ -149,7 +149,7 @@ class MoE(nn.Module):
             expert_outputs_NMD = torch.stack([expert(x_MD) for expert in self.experts], dim=0)
             expert_weights_MN = torch.zeros(x_MD.size(0), self.N, device=x_MD.device)
             expert_weights_MN.scatter_(1, idx_MK, val_MK.float())
-            y_MD = torch.einsum('mn,nmd->md', expert_weights_MN.bfloat16(), expert_outputs_NMD)
+            y_MD = torch.einsum('mn,nmd->md', expert_weights_MN, expert_outputs_NMD)
             counts_N = torch.bincount(idx_MK.flatten(), minlength=self.N).float()
         else:
             s_NM = einops.rearrange(scores_BSN, "B S N -> N (B S)")
